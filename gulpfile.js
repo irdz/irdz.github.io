49d97c40e8dfb7edb6fc3d7cf3636d7a9b79a0a2
Ivan Rodriguez <ivan.rodriguez001@gmail.com>
2015-12-31 17:54:13 -0800

    Fixed the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass')
+var sass = require('gulp-ruby-sass');
 
 gulp.task('sass', function(){
   return sass('styles.core.scss')
     .on('error', sass.logError)
     .pipe(gulp.dest('dest'));
-})
+});
 
 gulp.task('default', function(){
   // Code
