589d43143103323b84e6f1f6e2cf079a61d18f47
Ivan Rodriguez <ivan.rodriguez001@gmail.com>
2015-12-31 18:03:28 -0800

    Got the gulp job working for sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 
 gulp.task('sass', function(){
-  return sass('styles.core.scss')
+  return sass('styles/core.scss')
     .on('error', sass.logError)
     .pipe(gulp.dest('dest'));
 });
 
-gulp.task('default', function(){
-  // Code
-});
+gulp.task('default', ['sass']);
